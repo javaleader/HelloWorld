6f3b4c39dd31fcb14ddc759985a44cde9b257a33
javaleader <javaleader@163.com>
2016-11-09 15:55:14 +0800

    TestTwo
    测试代码

---
+++ TestTwo.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import java.util.Random;
+
+/**
+ * Created by david on 16/11/9.
+ */
+public class TestTwo {
+
+    public void three(){
+        int iRandomNum=new Random().nextInt(100)+1;
+        System.out.println("随机数:"+iRandomNum);
+    }
+    public static void main(String[] args) {
+        TestTwo two=new TestTwo();
+        two.three();
+    }
+}
